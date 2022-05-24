e9a4b340032b1f10ba7a5895c368532e40838f13
AndreyBukalo <bukaloandrey@gmail.com>
2022-05-24 20:37:55 +0300

    upd

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,59 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector(".gallery");
 
-console.log(galleryItems);
+gallery.addEventListener("click", onZoomImg); 
+
+
+
+
+const galleryMarkup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>`
+  )
+  .join("");
+  
+gallery.innerHTML = galleryMarkup; 
+
+function onZoomImg(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+  toggleGalleryModal();
+ 
+  };
+ 
+function toggleGalleryModal() {
+  const source = event.target.dataset.source;
+  const instance = basicLightbox.create(`
+    <img src="${source}"width="800" height="600">`);
+  instance.show();
+  gallery.addEventListener("keydown", (event) => {
+    if (event.key === "Escape") {
+      instance.close();
+    }
+    gallery.removeEventListener("keydown", event);
+    console.log("key: ", event.key);
+    console.log("code: ", event.code);
+  });
+};
+
+
+
+
+
+
+
+
+
